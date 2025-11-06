b560c698896fa2f233bef4c13718b7eba3a9aa02
Leonid Fiksler <l.fiksler@vkteam.ru>
2025-11-06 23:05:30 +0300

    feat(ci/cd) Обновил eslint

---
+++ eslint.config.js
@@ -0,0 +1,31 @@
+import eslintReact from "@eslint-react/eslint-plugin";
+import eslintJs from "@eslint/js";
+
+import {defineConfig} from "eslint/config";
+
+import tseslint from "typescript-eslint";
+
+export default defineConfig([
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    extends: [
+      eslintJs.configs.recommended,
+      tseslint.configs.recommended,
+      eslintReact.configs["recommended-typescript"],
+    ],
+    // Configure language/parsing options
+    languageOptions: {
+      // Use TypeScript ESLint parser for TypeScript files
+      parser: tseslint.parser,
+      parserOptions: {
+        // Enable project service for better TypeScript integration
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    // Custom rule overrides (modify rule levels or disable rules)
+    rules: {
+      "@typescript-eslint/no-unused-expressions": "warn",
+    },
+  },
+]);
