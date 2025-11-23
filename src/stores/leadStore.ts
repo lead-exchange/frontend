f78657ec4a2a4f6ddd265f7c7a47879cdf999acd
Leonid Fiksler <l.fiksler@vkteam.ru>
2025-11-23 16:46:01 +0300

    Added mobx

---
+++ src/stores/leadStore.ts
@@ -0,0 +1,24 @@
+import { Lead } from "@/types/entity";
+import { makeAutoObservable } from "mobx";
+
+class LeadStore {
+  leads: Lead[] = [];
+
+  constructor() {
+    makeAutoObservable(this);
+  }
+
+  setLeads(leads: Lead[]) {
+    this.leads = leads;
+  }
+
+  getLeadById(id: string): Lead | undefined {
+    return this.leads.find((lead) => lead.id === id);
+  }
+
+  getLeads(): Lead[] {
+    return this.leads;
+  }
+}
+
+export const leadStore = new LeadStore();
