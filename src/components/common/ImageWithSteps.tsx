3034af5192e9d8f5d61d4cb46dfba16792246e24
Leonid Fiksler <l.fiksler@vkteam.ru>
2025-11-29 16:07:36 +0300

    feat(ui) Added image placeholder

--- src/components/common/ImageWithSteps.tsx
+++ src/components/common/ImageWithSteps.tsx
@@ -3,6 +3,7 @@ import { type FC, useState } from 'react';
 import { Steps } from '@telegram-apps/telegram-ui';
 
 import './ImageWithSteps.css';
+import { Image } from 'lucide-react';
 
 interface ImageWithStepsProps {
   photos: string[];
@@ -11,10 +12,18 @@ interface ImageWithStepsProps {
 export const ImageWithSteps: FC<ImageWithStepsProps> = ({ photos }) => {
   const [activeIndex, setActiveIndex] = useState(0);
 
+  if (photos.length > 0) {
+    return (
+      <div className="image-w-steps" onClick={() => setActiveIndex(idx => (idx + 1) % photos.length)}>
+        {photos.length > 1 && <Steps className="image-w-steps__steps" count={photos.length} progress={activeIndex + 1} />}
+        <img className="image-w-steps__image" src={photos[activeIndex]} alt="some image" />
+      </div>
+    );
+  }
+
   return (
-    <div className="image-w-steps" onClick={() => setActiveIndex(idx => (idx + 1) % photos.length)}>
-      <Steps className="image-w-steps__steps" count={photos.length} progress={activeIndex + 1} />
-      <img className="image-w-steps__image" src={photos[activeIndex]} alt="some image" />
+    <div className="image-placeholder">
+      <Image size={96} strokeWidth={1} className="image-placeholder__icon" />
     </div>
   );
 };
--- src/requests/entities.ts
+++ src/requests/entities.ts
@@ -2,7 +2,7 @@ import { Lead, RealEstateObject } from '@/types/entity';
 import { apiClient } from './client';
 
 export const getLeads = async (userId: string): Promise<Lead[]> => {
-  const leads = await apiClient.get<Lead[]>(`/lead/${userId}`);
+  const leads = await apiClient.get<Lead[]>(`/api/leads/${userId}`);
   return leads.map(lead => ({
     ...lead,
     type: 'lead',
@@ -10,7 +10,7 @@ export const getLeads = async (userId: string): Promise<Lead[]> => {
 };
 
 export const getRealEstateObjects = async (userId: string): Promise<RealEstateObject[]> => {
-  const objects = await apiClient.get<RealEstateObject[]>(`/estate/${userId}`);
+  const objects = await apiClient.get<RealEstateObject[]>(`/api/estates/${userId}`);
   return objects.map(object => ({
     ...object,
     type: 'object',
